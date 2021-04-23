7f660627c3f78e27a0af25214aeeac8eb05d0b04
season91 <choayoung91@naver.com>
2021-04-23 18:40:50 +0900

    ㄱㅐ인작업

--- src/main/java/com/kh/toy/myCar/model/service/MyCarService.java
+++ src/main/java/com/kh/toy/myCar/model/service/MyCarService.java
@@ -5,8 +5,7 @@ import java.util.List;
 import com.kh.toy.car.model.vo.Car;
 
 public interface MyCarService {
-	// [등록 차량 조회]
-	// 1. 등록된 차량 정보 조회. 최대2개가 나올 수 있어 List로 받는다.
+
 	List<Car>  selectCarByGenerationIdx(String generationIdx);
 
 }
